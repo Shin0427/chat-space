12a3d118846a3d74dec48e5101f390645c3d42d0
Shin0427 <nishishin62@gmail.com>
2020-05-29 22:26:16 +0900

    added group functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "messages#index"
+  # root to: "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
 
